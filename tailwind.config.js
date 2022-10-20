eda0de9d751196153d06972bc4f858e4e108bf48
Nugraha <mangcoding@gmail.com>
2022-10-20 11:38:21 +0700

    Initial upload files
    Contoh header

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#99c6d8',
+            },
+            fontFamily: {
+                base: ['Nunito', 'sans-serif'],
+                nav: ['Raleway', 'sans-serif']
+            },
+            fontSize: {
+                'heading-1': '50px',
+            },
+        },
+    }
+}
